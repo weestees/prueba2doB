7e0e13e8b0469fc14a28054fa555e2fde1cbaabc
Alex Calderón <alex.calderon@epn.edu.ec>
2025-02-05 08:20:16 -0500

    instrumento

---
+++ Prueb2Ditritibuidas/Models/Instrumento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prueb2Ditritibuidas.Models
+{
+    public class Instrumento
+    {
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Tipo { get; set; }
+        public EstadoInstrumento Estado { get; set; }
+    }
+}
