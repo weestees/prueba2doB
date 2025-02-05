034fe687eb67df4ee2357953141f76fc22c0c179
Alex Calderón <alex.calderon@epn.edu.ec>
2025-02-05 08:22:26 -0500

    todo

---
+++ Prueb2Ditritibuidas/Models/EstadoInstrumento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prueb2Ditritibuidas.Models
+{
+    public enum EstadoInstrumento
+    {
+        Libre,
+        EnUso,
+        Dañado,
+        EnReparacion
+    }
+}
---
+++ Prueb2Ditritibuidas/Models/HistorialPrestamo.cs
@@ -0,0 +1,17 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prueb2Ditritibuidas.Models
+{
+    public class HistorialPrestamo
+    {
+        public string CodigoInstrumento { get; set; }
+        public DateTime FechaPrestamo { get; set; }
+        public DateTime? FechaDevolucion { get; set; }
+        public string Músico { get; set; }
+        public EstadoInstrumento EstadoAntes { get; set; }
+        public EstadoInstrumento EstadoDespues { get; set; }
+    }
+}
